1cda8085fffe9fa4c6c97fb755e14a3e74cdd853
chenbert1211 <chenbert1211@gmail.com>
2023-01-29 22:42:47 -0600

    added some css

--- client/components/Business-Account/MyResturant.js
+++ client/components/Business-Account/MyResturant.js
@@ -2,6 +2,22 @@ import React from 'react'
 import {connect} from 'react-redux'
 import {Link} from 'react-router-dom'
 import {getMyResturant} from '../../store/resturant'
+import Paper from '@mui/material/Paper'
+import Stack from '@mui/material/Stack'
+import {styled} from '@mui/material/styles'
+import {Button} from '@mui/material'
+
+const Item = styled(Paper)(({theme}) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  backgroundImage:
+    'url(http://riviera-buzz.com/wp-content/uploads/2016/05/terrasse-interior-1200.jpg)',
+  ...theme.typography.body2,
+  padding: theme.spacing(3),
+  textAlign: 'center',
+  height: '200px',
+  margin: '20px',
+  color: theme.palette.text.secondary
+}))
 
 class MyResturant extends React.Component {
   constructor(props) {
@@ -22,20 +38,30 @@ class MyResturant extends React.Component {
     let {resturant} = this.state
     return (
       <div>
-        <h1>My Resturants</h1>
-        <Link to="/addResturant">
-          <button>Add Resturant</button>
-        </Link>
-
-        {resturant
-          ? resturant.map((rest, ind) => {
-              return (
-                <Link to={`/edit-resturant/${rest.id}`}>
-                  <h1 key={ind}>{rest.name}</h1>
-                </Link>
-              )
-            })
-          : ''}
+        <div className="myResturant">
+          <h1>My Resturants</h1>
+          <Link to="/addResturant">
+            <Button>Add Resturant</Button>
+          </Link>
+        </div>
+        <div>
+          <Stack
+            direction="column"
+            justifyContent="center"
+            alignItems="stretch"
+            spacing={2}
+          >
+            {resturant
+              ? resturant.map((rest, ind) => {
+                  return (
+                    <Link to={`/edit-resturant/${rest.id}`}>
+                      <Item key={ind}>{rest.name}</Item>
+                    </Link>
+                  )
+                })
+              : ''}
+          </Stack>
+        </div>
       </div>
     )
   }
